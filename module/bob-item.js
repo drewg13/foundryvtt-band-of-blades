391f22d0343082f944933a356ca142c49b0647ba
agaringer <gariak@gmail.com>
2021-05-27 18:47:39 -0400

    properly delete flags on item delete

--- module/bob-item.js
+++ module/bob-item.js
@@ -5,9 +5,10 @@
 export class BoBItem extends Item {
 
   /** @override */
-  async _onCreate(data, options, user) {
+  async _onCreate( data, options, user ) {
     super._onCreate( data, options, user );
 
+    // Create actor flags for consumable uses dropdowns on sheet
     if( parseInt( data.data.uses ) ) {
       let key = data._id;
       let itemVal = parseInt( data.data.uses );
@@ -16,12 +17,24 @@ export class BoBItem extends Item {
         for( let i = 0; i <= itemVal; i++ ) {
           itemArray = itemArray + i;
         }
-        await this.parent.setFlag( "band-of-blades", "items." + key + ".usesarray", itemArray );
-        itemVal = 0;
-        itemArray = "";
+        await this.parent.setFlag( "band-of-blades", "items." + key + ".usesArray", itemArray );
       }
       await this.parent.setFlag( "band-of-blades", "items." + key + ".uses", data.data.uses );
-      await this.parent.setFlag( "band-of-blades", "items." + key + ".usesmax", data.data.uses );
+      await this.parent.setFlag( "band-of-blades", "items." + key + ".usesMax", data.data.uses );
+    }
+  }
+
+  /* -------------------------------------------- */
+
+  /** @override */
+  async _onDelete( options, user ) {
+    super._onDelete( options, user );
+
+    // Delete related flags on item delete
+    let itemFlag = await this.parent.getFlag( "band-of-blades", "items." + this.data._id ) || {};
+    if( itemFlag ) {
+      const key = "flags.band-of-blades.items.-=" + this.data._id;
+      this.parent.data.update({[key]: null})
     }
   }
 
